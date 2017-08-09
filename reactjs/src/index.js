dad0688a84ecaace3e5759f8410c2d7e296e65f9
Yongkang Du <duyongkang@jiandan100.cn>
2017-08-09 18:10:59 +0800

    第一章节

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ProductList from './ProductList';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ProductList />, document.getElementById('root'));
 registerServiceWorker();
